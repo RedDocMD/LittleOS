2275d56a9153b2b6a461f9078cefcf97b8d1cce1
Deep Majumder <deep.majumder2019@gmail.com>
2022-05-17 09:48:22 +0530

    Copied UART driver from tutorial

--- src/driver.rs
+++ src/driver.rs
@@ -1,12 +1,13 @@
-use self::{mini_uart::MINI_UART, qemu::QEMU_OUTPUT};
+use self::{mini_uart::MINI_UART, qemu::QEMU_OUTPUT, uart::PL011_UART};
 
 pub mod gpio;
 pub mod mini_uart;
 pub mod mmio;
 mod qemu;
+pub mod uart;
 
 pub fn console() -> &'static impl crate::print::Write {
-    &MINI_UART
+    &PL011_UART
 }
 
 pub fn qemu_console() -> &'static impl crate::print::Write {
@@ -17,7 +18,7 @@ pub trait DeviceDriver {
     fn init(&self);
 }
 
-static DRIVERS: [&'static (dyn DeviceDriver + Sync); 1] = [&MINI_UART];
+static DRIVERS: [&'static (dyn DeviceDriver + Sync); 2] = [&MINI_UART, &PL011_UART];
 
 pub fn drivers() -> &'static [&'static (dyn DeviceDriver + Sync)] {
     &DRIVERS
