7c87db9d52958f74e15b547e44126a8fb5fef03b
Deep Majumder <deep.majumder2019@gmail.com>
2022-06-02 10:06:19 +0530

    LIttle testing with a dynamic array!

--- src/main.rs
+++ src/main.rs
@@ -34,10 +34,22 @@ fn kernel_main() -> ! {
     kprintln!("Code end      : {:#018X}", mmu::layout::code_end());
 
     let alloc = BootAllocator::new();
-    match alloc.allocate_zeroed(Layout::array::<i32>(30).unwrap()) {
+    let arr_layout = Layout::array::<i32>(30).unwrap();
+    kprintln!("Allocating array ...");
+    match alloc.allocate_zeroed(arr_layout) {
         Ok(arr) => {
-            let arr = unsafe { arr.as_ref() };
+            let arr =
+                unsafe { &mut *core::ptr::slice_from_raw_parts_mut(arr.as_ptr() as *mut i32, 30) };
             kprintln!("Arr is of size {}", arr.len());
+            for i in (0..arr.len()).rev() {
+                arr[arr.len() - i - 1] = (i * i) as i32;
+            }
+            kprintln!("Array before sorting: {:?}", arr);
+            arr.sort_unstable();
+            kprintln!("Array after sorting: {:?}", arr);
+            let arr_ptr = core::ptr::NonNull::new(arr.as_mut_ptr() as *mut u8).unwrap();
+            unsafe { alloc.deallocate(arr_ptr, arr_layout) };
+            kprintln!("Deallocated array ...");
         }
         Err(_) => kprintln!("Failed to allocate 30 i32's"),
     }
